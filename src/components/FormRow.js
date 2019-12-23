10ccd5ee35a752ef5a23dee4015832235b145c8c
rivanildojr <rivanildo_junior7@hotmail.com>
2019-12-22 23:45:24 -0300

    Realizado o modulo de login

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -2,16 +2,33 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const FormRow = props => {
-  const { children } = props;
-  return <View style={styles.container}>{children}</View>;
+  const { children, first, last } = props;
+  return (
+    <View
+      style={[
+        styles.container,
+        first ? styles.first : null,
+        last ? styles.last : null
+      ]}
+    >
+      {children}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
     backgroundColor: "white",
-    margin: 5,
+    marginTop: 5,
+    marginBottom: 5,
     elevation: 2
+  },
+  first: {
+    marginTop: 10
+  },
+  last: {
+    marginBottom: 10
   }
 });
 
--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,7 +1,29 @@
 import React from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  Alert
+} from "react-native";
+
+import firebase from "@firebase/app";
+import "@firebase/auth";
 
 import FormRow from "../components/FormRow";
+
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID
+} from "react-native-dotenv";
 
 export default class LoginPage extends React.Component {
   constructor(props) {
@@ -9,18 +31,125 @@ export default class LoginPage extends React.Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      isLoading: false,
+      message: ""
     };
+  }
+
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABASE_URL,
+      projectId: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderId: MESSAGING_SENDER_ID,
+      appId: APP_ID,
+      measurementId: MEASUREMENT_ID
+    };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
   }
 
   onChangeHandler(field, value) {
     this.setState({ [field]: value });
   }
 
-  render() {
+  tryLogin() {
+    this.setState({ isLoading: true, message: "" });
+    const { email, password } = this.state;
+    console.log(email, password, "asdasdasdasda");
+
+    const loginUserSuccess = user => {
+      this.setState({ message: "Sucesso!" });
+    };
+
+    const loginUserFailed = error => {
+      this.setState({
+        message: this.getMessageByErrorCode(error.code)
+      });
+    };
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(loginUserSuccess)
+      .catch(error => {
+        console.log(error, "asdasddasdasdasdasd22");
+        if (error.code === "auth/user-not-found") {
+          Alert.alert(
+            "Usuário não encontrado",
+            "Deseja criar um cadastro com as informações inseridas?",
+            [
+              {
+                text: "Não",
+                style: "cancel" //IOS
+              },
+              {
+                text: "Sim",
+                onPress: () => {
+                  firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(loginUserSuccess)
+                    .catch(loginUserFailed);
+                }
+              }
+            ]
+          );
+          return;
+        }
+        loginUserFailed(error);
+      })
+      .then(() => this.setState({ isLoading: false }));
+  }
+
+  getMessageByErrorCode(errorCode) {
+    switch (errorCode) {
+      case "auth/wrong-password":
+        return "Senha incorreta";
+      case "auth/user-not-found":
+        return "Usuário não encontrado";
+      case "auth/invalid-email":
+        return "Email invalido";
+      default:
+        console.log(errorCode);
+        return "Erro desconhecido";
+    }
+  }
+
+  renderMessage() {
+    const { message } = this.state;
+
+    if (!message) return null;
+
     return (
       <View>
-        <FormRow>
+        <Text style={styles.mmm}>{message}</Text>
+      </View>
+    );
+  }
+
+  renderButton() {
+    if (this.state.isLoading) return <ActivityIndicator />;
+
+    return (
+      <Button
+        title="Entrar"
+        onPress={() => {
+          this.tryLogin();
+        }}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FormRow first>
           <TextInput
             style={styles.input}
             placeholder="user@mail.com"
@@ -28,7 +157,7 @@ export default class LoginPage extends React.Component {
             onChangeText={value => this.onChangeHandler("email", value)}
           />
         </FormRow>
-        <FormRow>
+        <FormRow last>
           <TextInput
             style={styles.input}
             placeholder="*******"
@@ -37,15 +166,23 @@ export default class LoginPage extends React.Component {
             onChangeText={value => this.onChangeHandler("password", value)}
           />
         </FormRow>
+        {this.renderButton()}
+        {this.renderMessage()}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10
+  },
   input: {
     paddingLeft: 5,
     paddingRight: 5,
     paddingBottom: 5
+  },
+  mmm: {
+    color: "red"
   }
 });
