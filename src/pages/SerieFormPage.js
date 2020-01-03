922cacfb0781acea6cc2426f181af4df4af89991
rivanildojr <rivanildo_junior7@hotmail.com>
2020-01-03 01:36:15 -0300

    Estruturando a tela com o componente ScrollView e arrumando o teclado com o componente KeyboardAvoidingView

--- src/pages/SerieFormPage.js
+++ src/pages/SerieFormPage.js
@@ -5,7 +5,10 @@ import {
   StyleSheet,
   TextInput,
   Picker,
-  Slider
+  Slider,
+  Button,
+  ScrollView,
+  KeyboardAvoidingView
 } from "react-native";
 
 import { connect } from "react-redux";
@@ -15,61 +18,70 @@ import { setField } from "../actions";
 
 const SerieFormPage = ({ serieForm, setField }) => {
   return (
-    <View>
-      <FormRow first>
-        <TextInput
-          style={styles.input}
-          placeholder="Título"
-          value={serieForm.title}
-          onChangeText={value => setField("title", value)}
-        />
-      </FormRow>
-      <FormRow>
-        <TextInput
-          style={styles.input}
-          placeholder="URL da Imagem"
-          value={serieForm.img}
-          onChangeText={value => setField("img", value)}
-        />
-      </FormRow>
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled
+      keyboardVerticalOffset={200}
+    >
+      <ScrollView>
+        <FormRow first>
+          <TextInput
+            style={styles.input}
+            placeholder="Título"
+            value={serieForm.title}
+            onChangeText={value => setField("title", value)}
+          />
+        </FormRow>
 
-      <FormRow>
-        <Picker
-          selectedValue={serieForm.gender}
-          onValueChange={itemValue => {
-            setField("gender", itemValue);
-          }}
-        >
-          <Picker.Item label="Policial" value="police" />
-          <Picker.Item label="Comédia" value="comedy" />
-          <Picker.Item label="Terror" value="horror" />
-        </Picker>
-      </FormRow>
+        <FormRow>
+          <TextInput
+            style={styles.input}
+            placeholder="URL da Imagem"
+            value={serieForm.img}
+            onChangeText={value => setField("img", value)}
+          />
+        </FormRow>
 
-      <FormRow>
-        <View style={styles.sameRow}>
-          <Text>Nota</Text>
-          <Text>{serieForm.rate}</Text>
-        </View>
-        <Slider
-          onValueChange={value => setField("rate", value)}
-          value={serieForm.rate}
-          maximumValue={100}
-          step={5}
-        />
-      </FormRow>
+        <FormRow>
+          <Picker
+            selectedValue={serieForm.gender}
+            onValueChange={itemValue => {
+              setField("gender", itemValue);
+            }}
+          >
+            <Picker.Item label="Policial" value="police" />
+            <Picker.Item label="Comédia" value="comedy" />
+            <Picker.Item label="Terror" value="horror" />
+          </Picker>
+        </FormRow>
 
-      <FormRow>
-        <TextInput
-          style={styles.input}
-          placeholder="Descrição"
-          value={serieForm.description}
-          onChangeText={value => setField("description", value)}
-          numberOfLines={4}
-          multiline={true}
-        />
-      </FormRow>
-    </View>
+        <FormRow>
+          <View style={styles.sameRow}>
+            <Text>Nota</Text>
+            <Text>{serieForm.rate}</Text>
+          </View>
+          <Slider
+            onValueChange={value => setField("rate", value)}
+            value={serieForm.rate}
+            maximumValue={100}
+            step={5}
+          />
+        </FormRow>
+
+        <FormRow>
+          <TextInput
+            style={styles.input}
+            placeholder="Descrição"
+            value={serieForm.description}
+            onChangeText={value => setField("description", value)}
+            numberOfLines={4}
+            multiline={true}
+          />
+        </FormRow>
+
+        <Button title="Salvar" onPress={() => console.log(serieForm)} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
